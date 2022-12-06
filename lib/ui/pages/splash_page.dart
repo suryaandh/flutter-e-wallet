dec18a2fbb511aa91911f1153fa7e3da0a4405b9
suryaant <gdswiyasa@gmail.com>
2022-12-06 08:32:32 +0800

    create splash page

---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ewallet/utils/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Container(
+          width: 155,
+          height: 150,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/img_logo_dark.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
